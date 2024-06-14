37cc614ff5b4c0162f3387b007acd0d18045340f
Diogo Centeno <CTW03642@criticaltechworks.com>
2024-06-14 11:46:38 +0100

    a

--- .github/actions/get_files.py
+++ .github/actions/get_files.py
@@ -4,9 +4,13 @@ def get_files() -> None:
     # List of file names to process
     file_names = os.listdir('files')
 
-    os.environ['FILES'] = ','.join(file_names)
+    #os.environ['FILES'] = ','.join(file_names)
+    env_file = os.getenv('GITHUB_ENV')
 
-    print(os.environ['FILES'])
+    with open(env_file, "a") as myfile:
+        myfile.write(f'FILES={file_names}')
+
+    #print(os.environ['FILES'])
 
 if __name__ == '__main__':
     get_files()
