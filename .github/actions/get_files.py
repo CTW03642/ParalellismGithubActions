c86deae64fea3a4f548624b6cedb23d7d24ae497
Diogo Centeno <CTW03642@criticaltechworks.com>
2024-06-14 12:04:26 +0100

    a

--- .github/actions/get_files.py
+++ .github/actions/get_files.py
@@ -4,14 +4,6 @@ def get_files() -> None:
     # List of file names to process
     file_names = os.listdir('files')
     print(' '.join(file_names))
-    return
-    #os.environ['FILES'] = ','.join(file_names)
-    env_file = os.getenv('GITHUB_ENV')
-
-    with open(env_file, "a") as myfile:
-        myfile.write(f"FILES={','.join(file_names)}")
-
-    #print(os.environ['FILES'])
 
 if __name__ == '__main__':
     get_files()
--- .github/actions/process_file.py
+++ .github/actions/process_file.py
@@ -1,13 +1,20 @@
 import argparse
-import os
-
-parser = argparse.ArgumentParser(description='Process a file.')
-parser.add_argument('--file-name', type=str, help='The name of the file to process')
-
-args = parser.parse_args()
 
 def process_file(file_name):
-    print(f"Processing file: {file_name}")
 
-print(os.environ['FILES'])
-process_file(args.file_name)
+    print(f"Processing file: {file_name}")
+    
+    with open(f'files/{file_name}', 'r') as file:
+        # Read the content of the file
+        file_content = file.read()
+         
+        # Print the content
+        print("File Content:\n", file_content)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Process a file.')
+    parser.add_argument('--file-name', type=str, help='The name of the file to process')
+
+    args = parser.parse_args()
+
+    process_file(args.file_name)
