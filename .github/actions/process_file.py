4467190ee02d13b8d4279ab4e5f2032f5e3ad11a
Diogo Centeno <CTW03642@criticaltechworks.com>
2024-06-14 11:32:49 +0100

    changes

--- .github/actions/process_file.py
+++ .github/actions/process_file.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 parser = argparse.ArgumentParser(description='Process a file.')
 parser.add_argument('--file-name', type=str, help='The name of the file to process')
@@ -8,4 +9,5 @@ args = parser.parse_args()
 def process_file(file_name):
     print(f"Processing file: {file_name}")
 
+print(os.environ['FILES'])
 process_file(args.file_name)
