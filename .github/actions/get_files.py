4bea72953ba5b6935560db54409ec9037f1e8953
Diogo Centeno <CTW03642@criticaltechworks.com>
2024-06-14 11:29:55 +0100

    changes

--- .github/actions/get_files.py
+++ .github/actions/get_files.py
@@ -4,7 +4,9 @@ def get_files() -> None:
     # List of file names to process
     file_names = os.listdir('files')
 
-    print(','.join(file_names))
+    os.environ['FILES'] = ','.join(file_names)
+
+    print(os.environ['FILES'])
 
 if __name__ == '__main__':
     get_files()
